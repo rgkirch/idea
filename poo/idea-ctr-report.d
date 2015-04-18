eca5195e063810f97df1f9e4eeeac08e97eb8667
Richie Kirchofer <rgkirch@gmail.com>
2015-04-17 20:55:10 -0400

    cleaned poo

--- poo/idea-ctr-report.d
+++
@@ -1,5 +0,0 @@
-# vim: ft=make
-.PHONY: idea-ctr-report._graphics
-idea-ctr-report.aux idea-ctr-report.aux.make idea-ctr-report.d idea-ctr-report.pdf: $(call path-norm,/usr/share/texlive/texmf-dist/tex/latex/base/article.cls)
-idea-ctr-report.aux idea-ctr-report.aux.make idea-ctr-report.d idea-ctr-report.pdf: $(call path-norm,idea-ctr-report.tex)
-.SECONDEXPANSION:
